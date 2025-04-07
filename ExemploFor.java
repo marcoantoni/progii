969e3c09717d39a5784e406bf9cfb94a6ca6fbcd
Marco Antoni <marco.antoni910@gmail.com>
2025-04-07 10:39:37 -0300

    Exemplo laço de repetição for

---
+++ ExemploFor.java
@@ -0,0 +1,13 @@
+class ExemploFor{
+	
+	public static void main(String args[]){
+		
+		// 1º condição de incialização da variavel
+		// 2º condição de saída da variavel
+		// 3º condição de incrementa da variavel que itera sobre o laço de repetição
+		for (int i=0; i < 10; i+=2) {
+			System.out.printf("Executando pela %d vez\n", i);
+		}
+		
+	}
+}
