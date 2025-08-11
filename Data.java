ab543cfe4bb46b4582fc3710e4c4f9b96b11fc10
Marco Antoni <marco.antoni910@gmail.com>
2025-08-11 11:24:16 -0300

    Add files via upload

--- Data.java
+++ Data.java
@@ -8,16 +8,6 @@ class Data {
 	
 	public Data (int dia, int mes, int ano) {
 		
-		// em 04/08 - foram adicionados valores default quando os passados valores inválidos para os atributos: Isso é para não aparecer o dia "0", o mês "0" e o ano "0" ao inserir um valor inválido dentro da data
-		
-		// testando se o dia é está num interlado válido
-		if (dia >= 1 && dia <= 31){
-			this.dia = dia;
-		} else {
-			System.out.printf("Dia inválido");
-			this.dia = 1;	// inicializando o dia com um valor padrão caso seja inválido
-		}
-			
 		if (mes >= 1 && mes <= 12){
 			this.mes = mes;
 		} else {
@@ -32,6 +22,27 @@ class Data {
 			System.out.printf("Ano inválid");
 			this.ano = 1970;	// iniciando o ano com um valor padrão caso seja inválido
 		}
+		
+		// para validar a quantidade de dias, primeiro é necessário o ano, pois o ano pode ser bissexto, e isso irá alterar a quantidade de dias de fevereirio.
+		// depois é necessário validar o mês, para saber a quantidade de dias que ele tem
+		
+		int diasMes[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		if (ehBissexto() == true) {
+			diasMes[2] = 29;	// quando o ano é bissexto, muda a quantidade de dias de fevereiro
+		} else {
+			diasMes[2] = 28;
+		}
+		
+		if (dia >= 1 && dia <= diasMes[this.mes]){
+			this.dia = dia;
+		} else {
+			System.out.printf("Dia inválido \n");
+			this.dia = 1;  // inicializando o mes com um valor padrão caso seja inválido
+		}
+		
+		
+	
 	}
 	
 	// método para escrever a data de maneira abreviada
@@ -41,6 +52,31 @@ class Data {
 		
 		return dia + "/" + mes + "/" + ano;
 	}
+	// método para escrever a data por extenso, no formato
+	// 11 de agosto de 2025
+	public String escreverPorExtenso(){
+		String meses[] = {"", "janeiro", "fevereiro", "março",
+			"abril", "maio", "junho", "julho", "agosto", "setembro",
+			"outubro", "novembro", "dezembro"};
+		
+		// criando a mensagem de saida
+		String saida = dia + " de " + meses[mes] + " de " + ano;
+		
+		return saida;
+		
+	}
+	
+	// criando um método para determinar se um ano é bissexto
+	// explicação sobre os anos bissextos: 
+	//https://brasilescola.uol.com.br/curiosidades/ano-bissexto.htm
+	public boolean ehBissexto(){
+		if ( (ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0){
+			// se isso for verdadeiro, o ano é bissexto
+			return true;
+		} else {
+			return false;
+		}	
+	}
 	
 	
 	// testando a classe
@@ -48,17 +84,21 @@ class Data {
 	public static void main(String args[]) {
 		Data hoje = new Data (23, 6, 2025);
 		
-		System.out.printf("Hoje é %s \n", hoje.escreverAbreviado() );
+		System.out.printf("Hoje é %s \n", hoje.escreverPorExtenso() );
 		
-		Data ferias = new Data(14, 7, 2025);
+		Data ferias = new Data(12, 12, 2025);
 		
-		System.out.printf("As férias começam em %s \n", 
-			ferias.escreverAbreviado() );
+		System.out.printf("As aulas terminam em %s \n", 
+			ferias.escreverPorExtenso() );
 			
 		Data avaliacao = new Data (25, 8, 2025);
 		
 		System.out.printf("A avaliação de Programação será em %s \n", 
-			avaliacao.escreverAbreviado() );
+			avaliacao.escreverPorExtenso() );
+			
+		Data testeBissexto = new Data(29, 2, 2024);
+		
+		System.out.printf("%s ", testeBissexto.escreverAbreviado() );
 	}
 
 	
