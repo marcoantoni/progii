da18c9a9091c98049564357e70ccc5d18b18f274
Marco Antoni <marco.antoni910@gmail.com>
2025-08-11 11:26:45 -0300

    Criando arrays com objetos
    Criação do array do tipo Aluno para representar a turma de estudantes

---
+++ AppTurma.java
@@ -0,0 +1,32 @@
+class AppTurma {
+
+	public static void main(String args[]) {
+		// criando alguns alunos para serem adicionados a turma
+		Aluno al1 = new Aluno("Régis", new Data(5,1, 2008), 
+			"Tec. em informática", "12345678910");
+			
+		Aluno al2 = new Aluno("Arthur Geske", new Data(25, 3, 2009),
+			"Tec. em informática", "78912378911");
+			
+		Aluno al3 = new Aluno("Andrei", new Data(28, 3, 2009),
+			"Tec. em informática", "78945612310");
+			
+		Aluno al4 = new Aluno("Nicolas", new Data(2, 2, 2008),
+			"Tec. em informática", "78945612310");
+			
+		Aluno al5 = new Aluno("Sofia", new Data(16, 7, 2008),
+			"Tec. em informática", "78945612310");
+			
+		// criando uma turma (um array de objetos do tipo Aluno)
+		Aluno turma[] = new Aluno[40];	// o array pode armazenar 40 alunos
+		// na hora de criar um array, se não for passar os valores, é necessário especificar o tamanho
+		
+		// adicionando os alunos na turma
+		turma[0] = al1; // adicionando o aluno1
+		turma[1] = al2;	// adicionando o aluno2
+		turma[2] = al3;	// adicionando o aluno3
+		turma[3] = al4;	// adicionando o aluno4
+		turma[4] = al5;	// adicionando o aluno5
+	}
+
+}
