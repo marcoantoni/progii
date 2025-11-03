29547697833aa9f3372ff248d8d7d9d311a63588
Marco Antoni <marco.antoni910@gmail.com>
2025-11-03 10:39:36 -0300

    Classe Pessoa - base para herança

---
+++ Pessoa.java
@@ -0,0 +1,43 @@
+public class Pessoa {
+	
+	protected String nome;
+	protected String cpf;
+	protected Data nascimento;
+	
+	// código sem método construtor
+	
+	/* Criação dos métodos sets para atribuir valores*/
+	
+	// o método set sempre tem a seguinte estrutura: public + void + set + Nome_atributo. É recebido um parametro que corresponde ao tipo de dado armazenado pelo atributo
+	public void setNome(String nome) {
+		// o nome para ser válido deve ter no mínimo duas letras
+		if (nome.length() > 2){
+			this.nome = nome;	// salvando o nome
+		} else {
+			System.out.printf("Nome inválido \n");
+		}
+	}
+	
+	public void setNascimento(Data nasc) {
+		// nascimento é um atributo do tipo Data
+		this.nascimento = nasc;	// armazena a data de nascimento
+	}
+	
+	public void setCPF(String cpf) {
+		// esse método só faz a atribuição sem validar
+		this.cpf = cpf;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public String getCPF(){
+		return cpf;
+	}
+	
+	public Data getNascimento(){
+		return nascimento;
+	}
+	
+}
